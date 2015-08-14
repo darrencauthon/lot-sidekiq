ea81da7018a59942f022e3114c0fba47542cf12e
Darren Cauthon <darren@cauthon.com>
2015-08-14 14:52:11 -0500

    Initial commit.

---
+++ lib/lot/sidekiq.rb
@@ -0,0 +1,7 @@
+require "lot/sidekiq/version"
+
+module Lot
+  module Sidekiq
+    # Your code goes here...
+  end
+end
---
+++ lib/lot/sidekiq/version.rb
@@ -0,0 +1,5 @@
+module Lot
+  module Sidekiq
+    VERSION = "0.0.1"
+  end
+end
