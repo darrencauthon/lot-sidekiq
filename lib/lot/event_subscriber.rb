a61c708ec79041edad5b2b6c84dc82f6618891bb
Darren Cauthon <darren@cauthon.com>
2015-08-14 16:20:08 -0500

    Turn an instigator into a string when passing it along the wire.

--- lib/lot/event_subscriber.rb
+++ lib/lot/event_subscriber.rb
@@ -3,7 +3,8 @@ module Lot
   class EventSubscriber
 
     def self.fire event, data, instigator
-      Lot::Sidekiq::Worker.perform_async 'OneEventSubscriber', event, data, nil
+      instigator = "#{instigator.record_type}:#{instigator.id}" if instigator
+      Lot::Sidekiq::Worker.perform_async 'OneEventSubscriber', event, data, instigator
     end
 
   end
--- spec/lot/event_subscriber_spec.rb
+++ spec/lot/event_subscriber_spec.rb
@@ -21,6 +21,19 @@ describe Lot::EventSubscriber do
 
     end
 
+    describe "with an instigator" do
+
+      let(:the_record_type) { random_string }
+      let(:the_id)          { random_string }
+      let(:the_instigator)  { Struct.new(:record_type, :id).new the_record_type, the_id }
+
+      it "should call perform_async with the relevant data" do
+        Lot::Sidekiq::Worker.expects(:perform_async).with 'OneEventSubscriber', the_event, the_data, "#{the_record_type}:#{the_id}"
+        OneEventSubscriber.fire the_event, the_data, the_instigator
+      end
+
+    end
+
   end
 
 end
